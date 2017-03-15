f6620ba59ea1931d41c3e882d6a75f1847ad3328
Victor Ernesto Velasco Esquivel <victor@macbook-pro-de-victor.local>
2017-03-14 21:43:29 -0600

    Initial Commit

---
+++ PizzaWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  PizzaWatch WatchKit Extension
+//
+//  Created by Victor Ernesto Velasco Esquivel on 14/03/17.
+//  Copyright © 2017 Victor Ernesto Velasco Esquivel. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
